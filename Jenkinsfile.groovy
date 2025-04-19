79c469b0aa0850efc9e453d5f766de5b1666fba2
sefi0 <sefi0609@gmail.com>
2025-04-19 17:42:09 +0300

    init commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building..'
-                sh ('docker version')
+                sh ('sudo docker version')
             }
         }
         stage('Test') {
