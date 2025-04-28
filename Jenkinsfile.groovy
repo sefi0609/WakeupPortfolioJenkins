9eb733c5127a0f69ba7ec93f2e00c0df6013a6ea
sefi0 <sefi0609@gmail.com>
2025-04-28 17:08:02 +0300

    test dockerfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,23 +18,24 @@
 }*/
 pipeline {
     agent { label 'linux_agents' }
+    environment { AWS_ECR = '340752809566.dkr.ecr.us-east-1.amazonaws.com' }
 
     stages {
         stage('Get credentials') {
             steps {
                 withCredentials([aws(credentialsId: "aws_credentials")]){
-                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 340752809566.dkr.ecr.us-east-1.amazonaws.com'
+                    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR'
                 }
             }
         }
         stage('Pull image') {
             steps {
-                sh 'docker pull 340752809566.dkr.ecr.us-east-1.amazonaws.com/automations:latest'
+                sh 'docker pull $AWS_ECR'
             }
         }
         stage('Run') {
             steps {
-                sh 'docker run -rm 340752809566.dkr.ecr.us-east-1.amazonaws.com/automations:latest'
+                sh 'docker run -rm $AWS_ECR'
             }
         }
     }
